f78f80f53540300d2ce006110f96760a01d40f80
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-04-03 20:48:31 -0400

    Update_project1.py

--- 602/project1.py
+++ 602/project1.py
@@ -12,3 +12,4 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRVUsZrUEFrV0_2OQSCIn_JHCgs-ylPlFyowhr63XTCyAebIpVp7Dzq4Os_ARfm0yeEsrkenL_he4K4/pub?gid=0&single=true&output=csv")
 print(data.head())
 
+sum(data)
