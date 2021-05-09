74c58ef80901c05c03815fa6a0e326c231f58f95
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-05-09 09:52:05 -0400

    Update_Project.py

--- 602/project1.py
+++ 602/project1.py
@@ -12,4 +12,12 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRVUsZrUEFrV0_2OQSCIn_JHCgs-ylPlFyowhr63XTCyAebIpVp7Dzq4Os_ARfm0yeEsrkenL_he4K4/pub?gid=0&single=true&output=csv")
 print(data.head())
 
-sum(data)
+print(data.columns)
+
+type(data['PlantWidth'])
+
+# Unable to remove due to LT0.5 in begining of string
+# ValueError: Unable to parse string "LT0.5" at position 0
+pd.to_numeric(data['PlantWidth'])
+
+# Must remove LT and impute something reasonable...
