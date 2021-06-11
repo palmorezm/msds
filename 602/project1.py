980e41c83095f3305786c5016cfad9df1fcc6e9d
palmorezm <palmorezm@users.noreply.github.com>
2021-06-10 20:09:15 -0400

    Update_Project1.py

--- 602/project1.py
+++ 602/project1.py
@@ -8,6 +8,7 @@ Created on Thu Mar 25 16:51:04 2021
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
 
 data = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRVUsZrUEFrV0_2OQSCIn_JHCgs-ylPlFyowhr63XTCyAebIpVp7Dzq4Os_ARfm0yeEsrkenL_he4K4/pub?gid=0&single=true&output=csv")
 print(data.head())
@@ -20,4 +21,8 @@ type(data['PlantWidth'])
 # ValueError: Unable to parse string "LT0.5" at position 0
 pd.to_numeric(data['PlantWidth'])
 
+
 # Must remove LT and impute something reasonable...
+ 
+    
+
