1e33a93c098abde0c11cc1f7d18891b05b43b606
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-07-08 19:14:14 -0400

    Update dataconversions.R

--- 624/Homework/dataconversions.R
+++ 624/Homework/dataconversions.R
@@ -14,14 +14,16 @@ write.csv(ChemicalManufacturingProcess,
 
 # Data Set: mlbench.friedman training data
 require(mlbench)
+require(Seurat)
 set.seed(200)
 trainingData <- mlbench.friedman1(200, sd = 1)
 trainingData$x <- data.frame(trainingData$x)
-featurePlot(trainingData$x, trainingData$y)
+FeaturePlot(trainingData$x, trainingData$y)
 write.csv(ChemicalManufacturingProcess, 
           "C:/data/624/ChemicalManufacturingProcess.csv")
 
 require(mlbench)
+require(Seurat)
 set.seed(200)
 testData <- mlbench.friedman1(5000, sd = 1)
 testData$x <- data.frame(testData$x)
