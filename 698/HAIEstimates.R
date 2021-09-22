fb73a3003ba31fed1ba300bce2b8797e8821aca3
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2021-09-21 21:45:50 -0400

    Create HAIEstimates.R

---
+++ 698/HAIEstimates.R
@@ -0,0 +1,11 @@
+
+# MS Analytics Capstone
+# Housing Affordability Index
+# Dataset: mixed
+# Sources: BEA and U.S.Census (ACS)
+# https://apps.bea.gov/iTable/
+# data.census.gov
+
+# Packages
+library(dplyr)
+library(tidyr)
