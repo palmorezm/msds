a54717260a5609e8512c46c8c743c5e49f680728
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2021-09-24 18:58:09 -0400

    Update_tutorialapps

--- 608/Mod3/greetingapp.R
+++ 608/Mod3/greetingapp.R
@@ -8,23 +8,24 @@
 #
 
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
-    numericInput("age", "How old are you?", value = NA),
-    textInput("name", "What's your name?"),
-    textOutput("greeting"),
-    tableOutput("mortgage")
-)
-
+     textInput("name", "What's your name?"),
+     numericInput("age", "How old are you?", value = updateNumericInput()),
+     textOutput("greeting"),
+     textOutput("age")
+ )
+ 
 server <- function(input, output, session) {
-                output$greeting <- renderText({
-                    paste0("Hello ", input$name)
-                })
+     output$greeting <- renderText({
+         paste0("Hello ", input$name)
+     })
+     output$age <- renderText({
+         paste("Are you really", input$age, "years old")
+     })
+ }
 
-                output$histogram <- renderPlot({
-                        hist(rnorm(1000))
-                    }, res = 96)
-}
-
-# Run the application 
 shinyApp(ui = ui, server = server)
+
+
