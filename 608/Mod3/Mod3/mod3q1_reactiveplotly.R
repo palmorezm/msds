477464602a4b675b3045699384ae4c7e786fb3b4
palmorezm <70336307+palmorezm@users.noreply.github.com>
2021-10-07 17:41:12 -0400

    Update mod3q1_reactiveplotly.R

--- 608/Mod3/Mod3/mod3q1_reactiveplotly.R
+++ 608/Mod3/Mod3/mod3q1_reactiveplotly.R
@@ -13,23 +13,58 @@ df <- data %>%
   group_by(State, ICD.Chapter) %>% 
   summarise(Crude_Mortality = sum(Deaths), 
             Population = sum(Population), 
-            Crude_Rate = Crude_Mortality / Population * 100000)
+            Crude_Rate = Crude_Mortality / Population * 100000) %>% 
+  arrange(desc(Crude_Rate))
 
 ui <- fluidPage(
   selectizeInput(
-    inputId = "Cause of Death", 
-    label = "Select Cause", 
+    inputId = "Cause", 
+    label = "Select Cause of Death", 
     choices = unique(df$ICD.Chapter), 
     selected = "Neoplasms", 
-    multiple = T
+    multiple = F
     ),
-    plotlyOutput(outputId = "p", height = "1000px", width = "1000px")
+    plotlyOutput(outputId = "p", height = "800px", width = "1000px")
   )
 
 server <- function(input, output, ...) {
   output$p <- renderPlotly({
-    plot_ly(df, x = ~Crude_Rate, y = ~State, type = "bar") 
+    df %>% 
+      filter(ICD.Chapter == input$Cause) %>%
+      arrange(desc(Crude_Rate)) %>% 
+      plot_ly(., 
+              x = ~Crude_Rate,
+              y = ~reorder(State, Crude_Rate), 
+              text = ~round(Crude_Rate, 0), 
+              textposition = 'auto', 
+              marker = list(color = 'rgb(150, 217, 250)',
+                            line = list(color = 'rgb(2,38,100)', width = 1.25))) %>% 
+      layout(title = "Crude Mortality Rate by State",
+             xaxis = list(title = "Crude Rate (Per 100,000)"),
+             yaxis = list(title = "State"))
+    
   })
 }
 
 shinyApp(ui, server)
+
+df %>% 
+  filter(ICD.Chapter == "Neoplasms") %>%
+  arrange(desc(Crude_Rate)) %>% 
+  plot_ly(., 
+          x = ~Crude_Rate,
+          y = ~reorder(State, Crude_Rate), 
+          text = ~round(Crude_Rate, 0), 
+          textposition = 'auto', 
+          marker = list(color = 'rgb(150, 217, 250)',
+                        line = list(color = 'rgb(2,38,100)', width = 1.25))) %>% 
+  layout(title = "Crude Mortality Rate by State",
+               xaxis = list(title = "Crude Rate (Per 100,000)"),
+               yaxis = list(title = "State"))
+
+
+
+
+
+
+
