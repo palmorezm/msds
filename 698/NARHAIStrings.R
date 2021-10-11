2802053e4dba90f7ef0feaa1ea433b3a004ef528
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2021-10-11 17:48:12 -0400

    Create NARHAIStrings.R

---
+++ 698/NARHAIStrings.R
@@ -0,0 +1,12 @@
+
+# MS Analytics Capstone
+# Extracting Information from Strings
+# Dataset: Affordability Index of Existing Single-Family Homes for Metropolitan Areas
+# Sources: Nationa Association of Realtors
+#   2017 - 2020:
+# https://cdn.nar.realtor/sites/default/files/documents/metro-affordability-2020-existing-single-family-2021-10-05.pdf
+#   2011 - 2014:
+# https://www.nar.realtor/sites/default/files/reports/2015/embargoes/2014-metro-affordability/metro-affordability-2014-existing-single-family-2015-02-11.pdf
+
+
+
