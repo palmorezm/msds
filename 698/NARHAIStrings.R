21bc411d16293c2006a64a2191bd2a7a47be17e3
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2021-10-15 13:44:50 -0400

    Update NARHAIStrings.R

--- 698/NARHAIStrings.R
+++ 698/NARHAIStrings.R
@@ -8,5 +8,14 @@
 #   2011 - 2014:
 # https://www.nar.realtor/sites/default/files/reports/2015/embargoes/2014-metro-affordability/metro-affordability-2014-existing-single-family-2015-02-11.pdf
 
+# Packages
+library(stringr)
+library(dplyr)
 
+fileName <- 'https://raw.githubusercontent.com/palmorezm/msds/main/698/Data/NARHAI2011_2014.txt'
+
+
+str <- read.delim(file = fileName)
+str_extract_all(str, pattern = "\\d{2}")
+str_extract(str, pattern = "\\d{")
 
