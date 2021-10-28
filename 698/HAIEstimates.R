97eb088a1ee9c8ebeab87d24f0c1754d5901a265
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2021-10-28 19:26:38 -0400

    RawEstimateEval

--- 698/HAIEstimates.R
+++ 698/HAIEstimates.R
@@ -10,47 +10,12 @@
 library(dplyr)
 library(tidyr)
 
-#### Start with Median Home Values ####
-mhv <- data.frame(read.csv("https://raw.githubusercontent.com/palmorezm/msds/main/698/Data/mhv.csv"))
-IR <- .035 # Select Interest Rate
+# Run Scripts In Order 
+# GEOID_GEOFIPS.R --> RPP.R --> IPD.R
+# Or use link from remote Git: 
+# 
 
-#### Bring in Median Income ####
-
-# Income, Population, & Per Capita Income by MSA from 2008 to 2019
-# Extraction
-cainc1 <- data.frame(read.csv(
-  "https://raw.githubusercontent.com/palmorezm/msds/main/698/Data/cainc1_msa_11yr.csv")) 
-cainc1 <- cainc1[4:nrow(cainc1),]
-colnames(cainc1) <- cainc1[1,]
-cainc1 <- cainc1[-1,]
-# Tidying
-minc <- cainc1 %>% 
-  gather(year, value, -GeoFips, 
-         -GeoName, -LineCode, -Description) 
-minc.personalincome <- subset(minc, LineCode==1)
-minc.population <- subset(minc, LineCode==2)  
-minc.percapitaincome<- subset(minc, LineCode==3) 
-minc <- rbind(minc.personalincome, minc.population, minc.percapitaincome)
-
-#### Combine Data ####
-
-minc %>% 
-  filter(GeoFips == "40060" & year == "2019")
-
-# per capita income is 58628 for Richmond, VA
-MEDINC <- 58628
-
-#### Create Estimates ####
-
-mhv %>% 
-  filter(GeoName == "Richmond, VA Metro Area") %>% 
-  mutate(PMT = MedianValue * 0.8 * (IR / 12)/(1 - (1/(1 + IR/12)^360)), 
-         QINC = PMT * 4 * 12, 
-         HAI = (MEDINC / QINC) * 100 )  
-# Median Income  (MEDINC) needs to be included in the final computation
-
-
-##### After running GEOID_GEOFIPS Mergers Script #####
+##### After Running Scripts #####
 
 # 384 Metros for 2019 Confirmed Reasonable HAI
 merged.df %>% 
@@ -68,8 +33,9 @@ df_mapping <- merged.df %>%
          PMT = MEDVAL * 0.8 * (IR / 12)/(1 - (1/(1 + IR/12)^360)), 
          QINC = PMT * 4 * 12,
          HAI = (MEDINC / QINC) * 100) %>%
-  filter(HAI >= 0 & HAI <= 500) %>% 
+  # filter(HAI >= 0 & HAI <= 500) %>% 
   arrange(desc(HAI)) 
+
 
 # 384 Metros for 2019 New Baseline HAI 
 # (minus average per person healthcare costs) - see link for details
