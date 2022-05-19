251daa756f64c1c8ad2c61cb2cbcfbea525f1dfc
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-05-18 20:57:45 -0500

    Create gettingstarted.R

---
+++ Bioinformatics and Genomes/gettingstarted.R
@@ -0,0 +1,19 @@
+
+# Computational Genomics with R 
+# Link: https://compgenomr.github.io/book/index.html
+
+
+
+
+### -----  Data  ----- ### 
+
+
+
+### ----- Packages ----- ### 
+
+# Install and Update Bioconductor Packages
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install()
+
+
