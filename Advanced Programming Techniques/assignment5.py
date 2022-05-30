1fce310eed393d4a387c76995d8c5705ef620221
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-05-30 11:48:42 -0500

    Add Getting Started Pkgs from Bioconductor

---
+++ Advanced Programming Techniques/assignment5.py
@@ -0,0 +1,16 @@
+
+# Where did assignment 5 go? 
+
+# core
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# ml
+from sklearn import datasets as ds
+from sklearn import linear_model as lm
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.model_selection import train_test_split as tts
+
+# infra
+import unittest
--- Bioinformatics and Genomes/gettingstarted.R
+++ Bioinformatics and Genomes/gettingstarted.R
@@ -2,18 +2,34 @@
 # Computational Genomics with R 
 # Link: https://compgenomr.github.io/book/index.html
 
-
-
-
-### -----  Data  ----- ### 
-
-
-
 ### ----- Packages ----- ### 
 
 # Install and Update Bioconductor Packages
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install()
+
+### Not Run!
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install(c('qvalue','plot3D','ggplot2','pheatmap','cowplot',
+#                        'cluster', 'NbClust', 'fastICA', 'NMF','matrixStats',
+#                        'Rtsne', 'mosaic', 'knitr', 'genomation',
+#                        'ggbio', 'Gviz', 'DESeq2', 'RUVSeq',
+#                        'gProfileR', 'ggfortify', 'corrplot',
+#                        'gage', 'EDASeq', 'citr', 'formatR',
+#                        'svglite', 'Rqc', 'ShortRead', 'QuasR',
+#                        'methylKit','FactoMineR', 'iClusterPlus',
+#                        'enrichR','caret','xgboost','glmnet',
+#                        'DALEX','kernlab','pROC','nnet','RANN',
+#                        'ranger','GenomeInfoDb', 'GenomicRanges',
+#                        'GenomicAlignments', 'ComplexHeatmap', 'circlize', 
+#                        'rtracklayer', 'BSgenome.Hsapiens.UCSC.hg38',
+#                        'BSgenome.Hsapiens.UCSC.hg19','tidyr',
+#                        'AnnotationHub', 'GenomicFeatures', 'normr',
+#                        'MotifDb', 'TFBSTools', 'rGADEM', 'JASPAR2018'
+# ))
+
+devtools::install_github("compgenomr/compGenomRData")
 
 
+### ----- End ----- ### 
