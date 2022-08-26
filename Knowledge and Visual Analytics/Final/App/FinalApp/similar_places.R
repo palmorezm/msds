10c0c40dc7b14e49618715fd61d58718c856cca8
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-08-25 21:14:13 -0500

    Pull 2019 at 500K

--- Knowledge and Visual Analytics/Final/App/FinalApp/similar_places.R
+++ Knowledge and Visual Analytics/Final/App/FinalApp/similar_places.R
@@ -1,5 +1,4 @@
 
-library(dplyr)
 source("C:/Users/Zachary Palmore/GitHub/msds/Knowledge and Visual Analytics/Final/App/FinalApp/df_source.R") # Might take a minute 
 
 df.fin %>%
@@ -7,7 +6,7 @@ df.fin %>%
 
 
 df.fin %>% 
-  filter(GeoName == "Janesville-Beloit, WI (Metropolitan Statistical Area)") 
+  filter(GeoName == "Janesville-Beloit, WI (Metropolitan Statistical Area)") %>% View()
 
 df.fin %>% 
   filter(HAI >= 142 & HAIRW >= 152) %>% View()
@@ -17,4 +16,7 @@ df.fin %>%
          year == 2019) %>% View()
 
 df.fin %>% 
-  filter(HAI >= 100) %>% View() 
+  filter(HAI >= 100, 
+         POP >= 500000, 
+         year == 2019) %>% 
+  dplyr::select(GeoName, year, POP, HAI, HAIRW, HAIRNT) %>% View() 
