7afdcffe0c94e1d5f8a9fd74ec81e075d2cbd2af
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2022-12-04 20:21:29 -0500

    Recovery of Stats

---
+++ Knowledge and Visual Analytics/SimpleExample2/app.R
@@ -0,0 +1,51 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(shiny)
+
+df <- read.csv('https://raw.githubusercontent.com/charleyferrari/CUNY_DATA608/master/lecture3/Sample%20Code/hpi.csv')
+df$DATE <- as.POSIXct(strptime(df$DATE, format = '%m/%d/%y'))
+
+ui <- fluidPage(
+    headerPanel('Housing Price Explorer'),
+    sidebarPanel(
+        selectInput('seas', 'Seasonality', unique(df$Seasonality), selected='SA'),
+        selectInput('metro', 'Metro Area', unique(df$Metro), selected='Atlanta'),
+        selectInput('tier', 'Housing Tier', unique(df$Tier), selected='High')
+    ),
+    mainPanel(
+        plotOutput('plot1'),
+        verbatimTextOutput('stats')
+    )
+)
+
+server <- function(input, output) {
+    
+    output$plot1 <- renderPlot({
+        
+        dfSlice <- df %>%
+            filter(Seasonality == input$seas, Metro == input$metro)
+        
+        ggplot(dfSlice, aes(x = DATE, y = HPI, color = Tier)) +
+            geom_line()
+    })
+    
+    output$stats <- renderPrint({
+        dfSlice <- df %>%
+            filter(Seasonality == input$seas, Metro == input$metro, Tier == input$tier)
+        
+        summary(dfSlice$HPI)
+    })
+    
+}
+
+shinyApp(ui = ui, server = server)
